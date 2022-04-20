352f2a7c9964f61849abb0d2381193764ba52551
Mikhail Nikitin <nitinin.miha@icloud.com>
2022-04-20 11:22:40 +0300

    Initial commit

---
+++ src/main/java/com/sainnt/homework6hibernate/Homework6HibernateApplication.java
@@ -0,0 +1,13 @@
+package com.sainnt.homework6hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework6HibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework6HibernateApplication.class, args);
+    }
+
+}
